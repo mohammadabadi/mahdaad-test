a42670ca72e069db9e063bd548840a6ae3617a75
Ali Mohammadabadi <mohammadabadi.project@gmail.com>
2025-08-19 18:22:00 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen flex items-start justify-center">
+    <p
+      class="px-6 py-2 text-white"
+    >
+    Ali Mohammadabadi | Frontend Developer
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
