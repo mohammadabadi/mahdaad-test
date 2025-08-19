600e9eca41972daaa59022926ea168ec674010e9
Ali Mohammadabadi <mohammadabadi.project@gmail.com>
2025-08-19 20:57:45 +0330

    finish task1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,61 @@
 <template>
-  <div class="min-h-screen flex items-start justify-center">
-    <p
-      class="px-6 py-2 text-white"
-    >
-    Ali Mohammadabadi | Frontend Developer
-    </p>
+  <div class="p-4 space-y-4">
+    <!-- theme Changer -->
+    <div class="flex items-center gap-2">
+      <span class="font-semibold text-gray-700 dark:text-white"> Theme :</span>
+      <button @click="theme = 'light'" class="px-3 py-1 rounded bg-gray-700">
+        Light
+      </button>
+      <button @click="theme = 'dark'" class="px-3 py-1 rounded bg-gray-700">
+        Dark
+      </button>
+    </div>
+    <!-- font size Changer -->
+    <div class="flex items-center gap-2">
+      <span class="font-semibold text-gray-700 dark:text-white"> Font Size :</span>
+      <button @click="fontSize = 'small'" class="px-3 py-1 rounded bg-gray-700">
+        small
+      </button>
+      <button @click="fontSize = 'medium'" class="px-3 py-1 rounded bg-gray-700">
+        medium
+      </button>
+      <button @click="fontSize = 'large'" class="px-3 py-1 rounded bg-gray-700">
+        large
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
+  import {ref,watch,onMounted} from "vue";
 
+  const theme = ref("light")
+  const fontSize = ref("medium")
+
+  const Storage = "user-preferences"
+
+  onMounted(()=>{
+    const saved = localStorage.getItem(Storage)
+    if(saved){
+      const parsed = JSON.parse(saved);
+      theme.value = parsed.theme || "light"
+      fontSize.value = parsed.fontSize || "medium";
+      applyPreferences()
+    }
+  });
+  watch ([theme,fontSize], () => {
+    localStorage.setItem(Storage,JSON.stringify({theme:theme.value,fontSize:fontSize.value}))
+    applyPreferences()
+  })
+  function applyPreferences(){
+    document.documentElement.classList.toggle("dark",theme.value === "dark")
+    document.documentElement.style.fontSize = fontSize.value === "small" ? "13px" : fontSize.value === "large" ? "22px" : "16px"
+  }
 </script>
 
 <style>
-
+html.dark{
+  background-color: #333333;
+  color:#ffffff;
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode : 'class', // for tailwind knows dark mode and dark class
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
