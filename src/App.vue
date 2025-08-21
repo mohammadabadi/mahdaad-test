6b40668d58e2a208586e445a2a8d0e2556f55749
Ali Mohammadabadi <mohammadabadi.project@gmail.com>
2025-08-21 13:09:21 +0330

    finish task 7

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="min-h-screen flex items-start justify-center">
-    <p
-      class="px-6 py-2 text-white"
-    >
-    Ali Mohammadabadi | Frontend Developer
-    </p>
+  <div class="flex gap-2 p-4 bg-gray-100 h-screen">
+    <div class="w-24 h-20 bg-red-500 text-white flex items-center justify-center rounded">
+      item 1
+    </div>
+    <div class="w-32 h-20 bg-blue-500 text-white flex items-center justify-center rounded">
+      item 2
+    </div>
+    <div class="flex-1 h-20 bg-green-500 text-white flex items-center justify-center rounded">
+      item 3
+    </div>
   </div>
 </template>
 
