cff7b44e52262972c048267b78c4a06b17f2297c
Ali Mohammadabadi <mohammadabadi.project@gmail.com>
2025-08-21 11:28:52 +0330

    finish task 4

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="min-h-screen flex items-start justify-center">
-    <p
-      class="px-6 py-2 text-white"
+  <div class="p-4">
+    <h2 class="text-2xl font-bold mb-4">User Activity Logs</h2>
+
+    <RecycleScroller
+      class="scroller border h-[500px] overflow-auto"
+      :items="logs"
+      :item-size="40"
+      key-field="id"
     >
-    Ali Mohammadabadi | Frontend Developer
-    </p>
+      <template #default="{item,index}">
+        <div class="p-2 border-b" :style="{minHeight:index % 5 === 0 ? '60px' : '40px'}">
+          {{ item.id }} - {{ item.message }}
+        </div>
+      </template>
+    </RecycleScroller>
   </div>
 </template>
 
 <script setup>
+import { RecycleScroller } from 'vue3-virtual-scroller'
+import {ref} from 'vue';
 
+const logs = ref(
+  Array.from({length:10000},(_,i) => ({
+    id:i+1,
+    message:`log entry #${i+1} - ${Math.random().toString(36).substring(2,15)}`
+  }))
+)
 </script>
 
 <style>
